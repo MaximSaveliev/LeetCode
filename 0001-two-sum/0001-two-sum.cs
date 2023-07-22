f0862ac78ed6d7a9d30dd6a5493cd9a7074a047b
MaximSaveliev <60519187+MaximSaveliev@users.noreply.github.com>
2023-07-22 18:59:34 +0300

    Time: 138 ms (97.35%), Space: 44.7 MB (29.92%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var pairs = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+            if (pairs.ContainsKey(target - nums[i]))
+                return new int[] { pairs[target - nums[i]], i };
+            else
+                pairs.TryAdd(nums[i], i);
+
+        return new int[] { -1, -1 };
+    }
+}
